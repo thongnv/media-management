3a0061293f79b35ce3fa6b7140533fc8ecbdfa35
thongnv <thongola@hotmail.com>
2017-10-27 14:41:02 +0700

    added files list page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET uploaded files page. */
+router.get('/uploads', function(req, res, next) {
+  res.render('uploads', { title: 'Express' });
+});
+
 module.exports = router;
