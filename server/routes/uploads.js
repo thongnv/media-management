d608da4bd7cf389917afaea95685bb5b37a266fb
Thong Nguyen <thongola@hotmail.com>
2018-02-06 00:16:50 +0700

    remove fake data

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let fs = require('fs');
 let router = express.Router();
-// const mediaController = require('../controllers').medias;
+const mediaController = require('../controllers').medias;
 
 /*------------- PAGE Rendering -------------*/
 
@@ -9,25 +9,20 @@ let router = express.Router();
 router.get('/', function (req, res, next) {
     let data = fs.readFileSync('public/data/sample-table-data.json', 'utf8');
     let obj = JSON.parse(data);
-    let records = obj.slice(0, 1000);
-    res.render('upload', {records: records, queryRecordCount: records.length, totalRecordCount: obj.length});
+    let records = obj.records;
+    res.render('upload', {records: records, queryRecordCount: records.length, totalRecordCount: obj.records.length});
 });
 
 /*------------- APIs -------------*/
 
 /* GET uploaded files listing. */
-router.get('/files', getFiles);
-
-function getFiles(req, res) {
-    let data = fs.readFileSync('public/data/sample-table-data.json', 'utf8');
-    let obj = JSON.parse(data);
-    res.status(200).json(obj);
-}
+router.get('/files', mediaController.list);
 
 /* DELETE an uploaded file. */
 router.delete('/:id', function(req, res, next) {
     console.log(req.params.id);
     res.json(req.params.id);
+    next();
 });
 
 module.exports = router;
