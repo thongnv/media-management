fcc65cea35000f8c8d249a14b46aa2ffec9c02e4
Thong Nguyen <thongola@hotmail.com>
2018-02-10 21:13:38 +0700

    update pagination

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -16,25 +16,41 @@ module.exports = {
             .catch(error => res.status(400).send(error));
     },
     list(req, res) {
-        return Media
-            .all()
-            .then(medias => {
-                let records = medias.slice(req.query.offset, req.query.offset + req.query.perPage * req.query.page);
+        const Op = sequelize.Op;
+        let query = req.query;
+        let queries = query.queries;
+        let results = [];
+        if (queries) {
+            results = Media.findAll({
+                offset: query.offset, limit: query.perPage,
+                where: {
+                    name: {
+                        [Op.iLike]: '%' + queries.search + '%'
+                    }
+                },
+            })
+        } else {
+            results = Media.findAll({offset: query.offset, limit: query.perPage});
+        }
+        return results.then(medias => {
+            Media.count().then(total => {
                 let data = {
-                    queryRecordCount: medias.length,
-                    totalRecordCount: medias.length,
+                    queryRecordCount: total,
+                    totalRecordCount: total,
                     records: []
                 };
-                for (let i = 0; i < records.length; i++) {
-                    let record = records[i].dataValues;
+                for (let i = 0; i < medias.length; i++) {
+                    let record = medias[i].dataValues;
                     record.date = '03/07/2017'; // just fake it for now
                     record.download = "<a href=" + record.name + " download><i class='fa fa-download' aria-hidden='true'></i></a>";
                     record.delete = "<a data-id=" + record.id + "><i class='fa fa-trash-o'></i></a>";
                     data.records.push(record);
                 }
-
                 res.status(200).json(data);
             })
-            .catch(error => res.status(400).send(error.stack));
-    },
+        }).catch(error => {
+            console.log(error);
+            res.status(400).send(error.stack)
+        });
+    }
 };
